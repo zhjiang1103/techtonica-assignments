0f49853dd264dde53953c0611ee188679d759eb0
zhjiang1103 <janetjiang1103@gmail.com>
2023-07-20 21:01:59 -0700

    Adding ingredients

--- Week1-Recipe/Recipe.js
+++ Week1-Recipe/Recipe.js
@@ -11,6 +11,19 @@
   <div>
       <image src="https://www.justonecookbook.com/wp-content/uploads/2021/01/Classic-Mentaiko-Pasta-3629-II.jpg" alt="Mentaiko Spaghetti" width="600" height="600">
      </div>
+     <div>
+        <h2>The Ingredients You’ll Need</h2>
+        <ul>
+          <li>Spicy salted pollock roe or cod roe (karashi mentaiko)</li>
+          <li>Whole milk</li>
+          <li>Heavy (whipping) cream</li>
+          <li>Unsalted butter</li>
+          <li>Soy sauce</li>
+          <li>Spaghetti</li>
+          <li>Kosher salt and black pepper</li>
+          <li>Toppings: shredded nori seaweed (kizami nori) and shiso leaves (perilla/ooba)</li>
+        </ul>
+      </div>
  
   </body>
   </html>
