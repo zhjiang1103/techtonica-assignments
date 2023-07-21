36a7de9c53d25ea564ca311f2e50e5091b80317f
zhjiang1103 <janetjiang1103@gmail.com>
2023-07-20 20:59:42 -0700

    Adding image

--- Week1-Recipe/Recipe.js
+++ Week1-Recipe/Recipe.js
@@ -8,7 +8,9 @@
     <h1>Mentaiko Spaghetti</h1>
       <p>A 15 mins ready dinner</p>
   </div>
-  
+  <div>
+      <image src="https://www.justonecookbook.com/wp-content/uploads/2021/01/Classic-Mentaiko-Pasta-3629-II.jpg" alt="Mentaiko Spaghetti" width="600" height="600">
+     </div>
  
   </body>
   </html>
