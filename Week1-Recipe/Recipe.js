88ea72ee432edbe8ccc81934d48ab1692ebb3ccf
zhjiang1103 <janetjiang1103@gmail.com>
2023-07-20 20:38:22 -0700

    First commit Recipe.js

---
+++ Week1-Recipe/Recipe.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Recipe Page</title>
+</head>
+<body>
+  <div>
+    <h1>Mentaiko Spaghetti</h1>
+      <p>A 15 mins ready dinner</p>
+  </div>
+ 
+  </body>
+  </html>
+   //First version
+   //First commit
