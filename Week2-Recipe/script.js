c629b3bfb66d0f3469452bc061d01e71b7c46892
zhjiang1103 <janetjiang1103@gmail.com>
2023-07-27 10:39:11 -0700

    Adding checkbox to ingredients

--- Week2-Recipe/script.js
+++ Week2-Recipe/script.js
@@ -1,3 +1,4 @@
+//Adding ingredient to the list
 // First up, let's create a new list item and store it in a variable.
 var newListItem = document.createElement('li');
 
@@ -8,5 +9,20 @@ newListItem.textContent = 'Nori seaweed strips';
 document.querySelector('ul').appendChild(newListItem);
 
 
+//Adding checkbox
+// Step 1: Select the list element
+const listElement = document.getElementById("myList").getElementsByTagName("ul");
+// Step 2: Iterate through list items
+const listItems = listElement.getElementsByTagName("li");
+for (let i = 0; i < listItems.length; i++) {
+    // Step 3: Create a new checkbox for each list item
+    const newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCheckbox.name = "name";
+    newCheckbox.value = "value";
+    newCheckbox.id = "id";
+    //Step 4: Attach the checkbox to the list item
+            listItems[i].appendChild(newCheckbox);
+}
 
 
