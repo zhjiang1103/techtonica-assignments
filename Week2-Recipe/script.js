44cb34c4c2441ee7c74a6bb0abf126448d5ddc2e
zhjiang1103 <janetjiang1103@gmail.com>
2023-07-27 10:20:36 -0700

    Adding one more ingredient the list

---
+++ Week2-Recipe/script.js
@@ -0,0 +1,12 @@
+// First up, let's create a new list item and store it in a variable.
+var newListItem = document.createElement('li');
+
+// Now let's update the text content of that list item.
+newListItem.textContent = 'Nori seaweed strips';
+
+// And finally, let's add that list item as a child of the ul.
+document.querySelector('ul').appendChild(newListItem);
+
+
+
+
