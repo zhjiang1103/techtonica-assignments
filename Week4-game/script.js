74be8cdcbc7669d7067dcf59327fcc1e1b793eac
Janet Jiang <janetjiang1103@gmail.com>
2023-08-11 11:32:40 -0700

    Clean up error message once user had new input

--- Week4-game/script.js
+++ Week4-game/script.js
@@ -4,15 +4,15 @@ document.addEventListener("DOMContentLoaded", () => {
   
     Button.addEventListener("click", () => {
       // Toggle the visibility of the picture container
-      pictureContainer.style.display = pictureContainer.style.display === "none" ? "block" : "none";
+      pictureContainer.style.display = pictureContainer.style.display !== "block" ? "block" : "none";
   
       // Load and display the picture
-      if (pictureContainer.style.display === "block") {
-        const picture = document.createElement("img");
-        picture.src = "https://freight.cargo.site/w/1080/q/94/i/8c26cb1e6d8b7aead75057ad75428318b5e604d054e018e62d7b3a628c6bb70b/coinflip_01.gif" // Replace with the actual image path
-        pictureContainer.innerHTML = ""; // Clear any existing content
-        pictureContainer.appendChild(picture);
-      }
+      // if (pictureContainer.style.display === "block") {
+      //   const picture = document.createElement("img");
+      //   picture.src = "https://freight.cargo.site/w/1080/q/94/i/8c26cb1e6d8b7aead75057ad75428318b5e604d054e018e62d7b3a628c6bb70b/coinflip_01.gif" // Replace with the actual image path
+      //   pictureContainer.innerHTML = ""; // Clear any existing content
+      //   pictureContainer.appendChild(picture);
+      // }
     });
   })
 
@@ -24,6 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
   function compareResult() {
     let input = document.getElementById("guess").value;
+    errorMessage.innerHTML="";
     let coinFlip = Math.round(Math.random());
     let userGuess; 
     if (input==="Tail" || input==="tail"){ userGuess=0 }
