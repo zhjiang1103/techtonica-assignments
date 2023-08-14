abf1a0c7927c27165f49933c55985873abf2b559
Janet Jiang <janetjiang1103@gmail.com>
2023-08-14 12:12:03 -0700

    Removing picture source from JS to HTML

--- Week4-game/script.js
+++ Week4-game/script.js
@@ -6,13 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
       // Toggle the visibility of the picture container
       pictureContainer.style.display = pictureContainer.style.display !== "block" ? "block" : "none";
   
-      // Load and display the picture
-      // if (pictureContainer.style.display === "block") {
-      //   const picture = document.createElement("img");
-      //   picture.src = "https://freight.cargo.site/w/1080/q/94/i/8c26cb1e6d8b7aead75057ad75428318b5e604d054e018e62d7b3a628c6bb70b/coinflip_01.gif" // Replace with the actual image path
-      //   pictureContainer.innerHTML = ""; // Clear any existing content
-      //   pictureContainer.appendChild(picture);
-      // }
+      
     });
   })
 
